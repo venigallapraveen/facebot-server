ee4adea5a66f91c0b53fc1ef16c5a9b885eb37b0
Praveen Venigalla <39606861+venigallapraveen@users.noreply.github.com>
2021-08-18 15:36:13 -0400

    Update server.js

--- server.js
+++ server.js
@@ -24,13 +24,10 @@ app.use(cors());
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://praveen-facebot.herokuapp.com');
+    res.setHeader('Access-Control-Allow-Origin', '*);
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
     // Pass to next layer of middleware
     next();
