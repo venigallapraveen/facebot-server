dc645bce6607869609eb92aea2ffef54ac6647ef
Praveen Venigalla <39606861+venigallapraveen@users.noreply.github.com>
2021-08-18 15:47:43 -0400

    Update server.js

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(cors());
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*);
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
